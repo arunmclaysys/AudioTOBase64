bebf5674d11890c811ddcdd62ee6c6038f3da918
manoj.mazhuvendhi <manoj.mazhuvendhi@SYSLP965>
2024-01-29 12:34:40 +0530

    Add project files.

---
+++ AudioTOBase64/Controllers/AudioController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text;
+
+namespace AudioTOBase64.Controllers
+{
+    public class AudioController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> UploadAudioFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadAudioFile([FromForm] Models.Class model)
+        {
+            if (model == null || model.File == null || model.File.Length == 0)
+            {
+                return BadRequest("Invalid file");
+            }
+
+            try
+            {
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    await model.File.CopyToAsync(memoryStream);
+
+                    
+                    string base64String = Convert.ToBase64String(memoryStream.ToArray());
+
+                    return Ok(base64String);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+       
+    }
+}
---
+++ AudioTOBase64/Models/Class.cs
@@ -0,0 +1,7 @@
+﻿namespace AudioTOBase64.Models
+{
+    public class Class
+    {
+        public IFormFile File { get; set; }
+    }
+}
